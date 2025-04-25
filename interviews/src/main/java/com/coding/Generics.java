2eec3a9d405e6eee453bfa810adf166fecb9f839
Akash_Roy <akash.roy2@ihsmarkit.com>
2025-04-25 08:37:02 +0200

    adding Generic code

--- interviews/src/main/java/com/coding/Generics.java
+++ interviews/src/main/java/com/coding/Generics.java
@@ -3,9 +3,65 @@ package com.coding;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.util.stream.Stream;
+
+// create a Generic Itertor
+interface  BaseIterator<T>{
+           boolean hasPrevious();
+           boolean hasNext();
+           T next();
+}
+
+class DataIterator<T> implements  BaseIterator<T>{
+    int currentIndex= 0;
+    int previousIndex =0;
+    T data[];
+    public DataIterator ( T data[]){
+             this.data = data;
+    }
+
+    @Override
+    public boolean hasPrevious () {
+        if (previousIndex == 0){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean hasNext () {
+        if (data.length-1 == currentIndex){
+                 return false;
+        }
+        return true;
+    }
+
+    @Override
+    public T next () {
+        return  data[currentIndex];
+    }
+}
+
+
+
 
 public class Generics {
 
+    // Utility  method to create a generic method that converts tandnArray to List
+    public static  <T> List<T>  toList (T arr[]){
+            List<T> list = new ArrayList<>();
+        for ( T   element : arr){
+                 list.add(element);
+             }
+        return list;
+    }
+
+    public static void main (String args[]){
+        Generics generics = new Generics();
+        Generics.toList( new Integer[]{ 10 ,20 , 30 , 40 , 50, 60, 70}).stream().forEach( System.out ::println);
+
+    }
 
 
 }
